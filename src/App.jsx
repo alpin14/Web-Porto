0db58dd2a7b516f2f6030a636560f374219ea760
Alpin Yuda Warija Rambe <alpinyuda14@gmail.com>
2025-02-24 05:16:29 +0700

    <Feature> [Loading Screen2]-Menambahkan efek blow pada loading screen

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,15 @@ import "./index.css";
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
   return (
-    <>{!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}</>
+    <>
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
+      {""}
+      <div
+        className={`min-h-screen transition-opacity duration-700 ${
+          isLoaded ? "opacity-100" : "opacity-0"
+        } bg-black text-gray-100`}
+      ></div>
+    </>
   );
 }
 
